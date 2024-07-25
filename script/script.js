394ebc13df725ebb4c3a3176ac1fce5fac633102
Aayush <aaysuhadhikari70@gmail.com>
2024-07-25 13:18:13 +0545

    aaduroprem

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const addButton = document.querySelector('#add');
+const bodyDiv = document.querySelector('.bodyDiv')
 
 const newNote = (text = '') => {
+    
     const mainDiv = document.createElement('div');
     mainDiv.classList.add('divvv');
 
@@ -32,58 +34,11 @@ const newNote = (text = '') => {
     });
 
     mainDiv.appendChild(note);
-    document.body.appendChild(mainDiv)
+    bodyDiv.appendChild(mainDiv);
 };
+    
 
 addButton.addEventListener('click', newNote);
 
 
 
-
-
-
-
-
-
-
-
-
-// const addButton = document.querySelector('#add');
-
-// const newNote = (text = '') => {
-//     const mainDiv = document.createElement('div');
-//     mainDiv.classList.add('flex');
-
-//     const note = document.createElement('div');
-//     note.classList.add('note', 'relative', 'bg-slate-600', 'h-[300px]', 'w-[400px]', 'flex', 'mx-16');
-
-//     const htmlData =`
-//         <div class="operation absolute top-2 left-[300px]">
-//             <button class="edit"><i class="fa-solid fa-file-pen text-3xl hover:bg-blue-500 rounded-lg"></i></button>
-//             <button class="delete"><i class="fa-solid fa-trash text-3xl hover:bg-red-500 rounded-lg"></i></button>
-//         </div>
-//         <div class="main hidden"></div>
-//         <textarea class="absolute bg-black text-white right-[30px] top-[70px] p-[80px]">${text}</textarea>`
-//     ;
-//     note.insertAdjacentHTML('afterbegin', htmlData);
-
-//     const editBtn = note.querySelector('.edit');
-//     const deleteBtn = note.querySelector('.delete');
-//     const main = note.querySelector('.main');
-//     const textArea = note.querySelector('textarea');
-
-
-
-//     // Editing a note
-
-
-//     // Deleting a note
-//     deleteBtn.addEventListener('click', () => {
-//         note.remove();
-//     });
-
-//     mainDiv.appendChild(note);
-//     document.body.appendChild(mainDiv);
-// };
-
-// addButton.addEventListener('click', () => newNote());
